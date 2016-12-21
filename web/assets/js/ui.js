3fc639a0573e1d36ab823fe4ab01428a78238d05
Navit Kumar <navit@intelligent.com>
2016-12-22 02:55:59 +0530

    my preferences and reset password js and css

--- web/assets/js/ui.js
+++ web/assets/js/ui.js
@@ -30,7 +30,8 @@ function Login() {
 }
 
 $.extend(Login, {
-    ajaxLink: window.location.origin + '/api/v1/general'
+    ajaxLink: window.location.origin + '/api/v1/general',
+    ajaxLink2: window.location.origin + '/api/v1'
 });
 
 $.extend(Login.prototype, {
@@ -40,6 +41,12 @@ $.extend(Login.prototype, {
         that.flipForm();
         that.featureSlider();
         that.openMenu();
+        if($('#mypreferences').length){
+            that.updatePreferences();
+        }
+        if($('#change_password').length){
+            that.changePassword();
+        }
     },
     bindForgetPasswordAction: function () {
         var that = this;
@@ -128,6 +135,133 @@ $.extend(Login.prototype, {
             }
         });
 
+    },
+    updatePreferences : function (){
+        $('#update_preferences').click(function(){
+            var _fname = $.trim($('#full_name_text').val());
+            var error = 'Name field can not be empty';
+            if(_fname == ''){
+                $('.validation_msg').html(error);
+                $('.validation_msg').show();
+            }
+            else{
+                var obj = {
+                        head: {
+                            action: "changePreference"
+                        },
+                        body: {
+                            name: _fname
+                        }
+                    };
+                var _str = JSON.stringify(obj);
+                $.ajax({
+                    url: Login.ajaxLink2,
+                    method: 'post',
+                    dataType: 'json',
+                    data: _str,
+                    beforeSend: function () {
+                        $('#loader').show();
+                    },
+                    success: function(res){
+                        var msg = '';
+                        $('#loader').hide();
+                        if (res.head.status === "success") {
+                            msg = '<span class="msg">Your name has been successfully updated</span>';
+                        }
+                        $('.validation_msg').html('');
+                        $('#valid_error').html('');
+                        $('#valid_error').html(msg);
+                    },
+                    error: function(err){
+                        var error = '';
+                        $('#loader').hide();
+                        obj = jQuery.parseJSON(err.responseText);
+
+                        if (obj.head.status === 'error') {
+                            error = '<span class="error_msg">' + obj.body.error_msg + '</span>';
+                        }
+                        $('#valid_error').html('');
+                        $('#valid_error').html(error);
+                    }
+                });   
+            }
+        });
+    },
+    changePassword: function(){
+        $('#change_password_btn').click(function(){
+            
+            var error = '';
+            var _oldPassword = $('#old_password').val();
+            var _newPassword = $('#new_password').val();
+            var _renewPassword = $('#re_new_password').val();
+            $('#valid_error').html('');
+            $('.validation_msg').html('');
+            $('.validation_msg2').html('');
+            if(_oldPassword == ''){
+                error = 'Old Password field can not be empty';
+                $('.validation_msg').html(error);
+                $('.validation_msg').show();
+            }
+            else if(_newPassword !==  _renewPassword){
+                $('.validation_msg2').html('Retype password is not matching');
+                $('.validation_msg2').show();
+            }
+            else if(_newPassword == '' || _renewPassword == ''){
+                $('.validation_msg2').html('New password fields can not empty');
+                $('.validation_msg2').show();
+            }
+            else{
+                $('.validation_msg2').html('');
+                $('.validation_msg').html('');
+                
+                var obj = {
+                        head: {
+                            action: "changePassword"
+                        },
+                        body: {
+                            old_password: _oldPassword,
+                            new_password: _renewPassword
+                        }
+                    };
+                var _str = JSON.stringify(obj);
+                $.ajax({
+                    url: Login.ajaxLink2,
+                    method: 'post',
+                    dataType: 'json',
+                    data: _str,
+                    beforeSend: function () {
+                        $('#loader').show();
+                    },
+                    success: function(res){
+                        var msg = '';
+                        $('#loader').hide();
+                        if (res.head.status === "success") {
+                            msg = '<span class="msg">Your password has been successfully updated</span>';
+                        }
+                        $('.validation_msg').html('');
+                        $('#valid_error').html('');
+                        $('#valid_error').html(msg);
+                        setTimeout(function(){
+                            window.location.href = 'http://'+window.location.hostname+'/login';
+                        },1000);
+                        
+                    },
+                    error: function(err){
+                        var error = '';
+                        $('#loader').hide();
+                        obj = jQuery.parseJSON(err.responseText);
+                        
+                        if (obj.head.status === 'error') {
+                            error = '<span class="error_msg">' + obj.body.error_msg + '</span>';
+                        }
+                        $('#valid_error').html('');
+                        $('#valid_error').html(error);
+                    }
+                }); 
+            }
+            
+            
+        });
     },
     fieldValidate: function (valobj, field) {
 
