1ed7ee1ccdce4bc545789efbea5406ba997fbd9c
Navit Kumar <navit@intelligent.com>
2016-12-25 00:18:49 +0530

    create role js finished

--- web/assets/js/ui.js
+++ web/assets/js/ui.js
@@ -4,27 +4,29 @@
  * and open the template in the editor.
  */
 
+function Login() {
+}
+
 function User() {
 }
 
 function Role() {
 }
 
-$.extend(User, {
+$.extend(Login,{
     ajaxLink: window.location.origin + '/api/v1/general',
     ajaxLink2: window.location.origin + '/api/v1',
-    roles: ['Registered', 'Unregistered', 'Unverified', 'Deactivated', 'Denied', 'Password Reset'],
-    role: new Role()
+    role: new Role(),
+    user: new User()
 });
 
-$.extend(User.prototype, {
-    init: function () {
+$.extend(Login.prototype,{
+    
+    init: function(){
         var that = this;
         that.bindForgetPasswordAction();
         that.flipForm();
-        that.featureSlider();
         that.openMenu();
-        User.role.init();
         if ($('#mypreferences').length) {
             that.updatePreferences();
         }
@@ -33,53 +35,6 @@ $.extend(User.prototype, {
         }
         if ($('#registration').length) {
             that.registration();
-        }
-        if ($('#userList').length) {
-            that.createUserPage();
-        }
-
-    },
-    bindForgetPasswordAction: function () {
-        var that = this;
-        $('#forget_password_btn').click(function () {
-            var field_validated = that.fieldValidate($('#forget_password_textbox'), 'email');
-
-            if (field_validated === true) {
-                that.getForgetPassword($('#forget_password_textbox').val());
-            }
-        });
-    },
-    featureSlider: function () {
-        if ($('.feature-list').length) {
-            var li_width = 30;
-            $('.feature-list ul li').each(function () {
-                li_width = li_width + $(this).outerWidth();
-            });
-            $('.feature-list ul').css('width', li_width + 'px');
-            $('.next').click(function () {
-
-                var diff = $('.feature-list ul').width() - $(window).width();
-                if (diff > 0) {
-                    $('.feature-list ul').animate({'left': '-' + diff + 'px'}, 500);
-                }
-            });
-            $('.prev').click(function () {
-
-                var diff = $('.feature-list ul').width() - $(window).width();
-                var pos = 0;
-                if (diff > 0) {
-                    var left_pos = $('.feature-list ul').css('left');
-                    left_pos = parseInt(left_pos);
-
-                    if (left_pos == 0) {
-                        pos = left_pos;
-                    }
-                    else {
-                        pos = diff + left_pos;
-                    }
-                    $('.feature-list ul').animate({'left': '+' + pos + 'px'}, 500);
-                }
-            });
         }
     },
     getForgetPassword: function (_val) {
@@ -93,8 +48,8 @@ $.extend(User.prototype, {
             }
         };
         var _obj = JSON.stringify(obj);
-        var ajaxLink = User.ajaxLink;
-        that.getAjaxData(ajaxLink, _obj, function (_data) {
+        var ajaxLink = Login.ajaxLink;
+        Login.user.getAjaxData(ajaxLink, _obj, function (_data) {
 
             var msg = '';
             $('#loader').hide();
@@ -106,6 +61,33 @@ $.extend(User.prototype, {
             $('#valid_error').html('');
             $('#valid_error').html(msg);
         });
+    },
+    bindForgetPasswordAction: function () {
+        var that = this;
+        $('#forget_password_btn').click(function () {
+            var field_validated = that.fieldValidate($('#forget_password_textbox'), 'email');
+
+            if (field_validated === true) {
+                that.getForgetPassword($('#forget_password_textbox').val());
+            }
+        });
+    },
+    flipForm: function () {
+        if ($('.flip').length) {
+            $('.flip').click(function () {
+                $('.card').toggleClass('flipped');
+                return false;
+            });
+        }
+    },
+    openMenu: function () {
+        if ($('.open-menu').length) {
+            $('.open-menu').click(function () {
+                var id = $(this).attr('href');
+                $(id).slideToggle();
+                return false;
+            });
+        }
     },
     updatePreferences: function () {
         var that = this;
@@ -126,8 +108,8 @@ $.extend(User.prototype, {
                     }
                 };
                 var _obj = JSON.stringify(obj);
-                var ajaxLink = User.ajaxLink2;
-                that.getAjaxData(ajaxLink, _obj, function (_data) {
+                var ajaxLink = Login.ajaxLink2;
+                Login.user.getAjaxData(ajaxLink, _obj, function (_data) {
 
                     var msg = '';
                     $('#loader').hide();
@@ -180,8 +162,8 @@ $.extend(User.prototype, {
                     }
                 };
                 var _obj = JSON.stringify(obj);
-                var ajaxLink = User.ajaxLink2;
-                that.getAjaxData(ajaxLink, _obj, function (_data) {
+                var ajaxLink = Login.ajaxLink2;
+                Login.user.getAjaxData(ajaxLink, _obj, function (_data) {
                     var msg = '';
                     $('#loader').hide();
                     if (_data.head.status === "success") {
@@ -194,10 +176,7 @@ $.extend(User.prototype, {
                         window.location.href = 'http://' + window.location.hostname + '/login';
                     }, 1000);
                 });
-
             }
-
-
         });
     },
     registration: function () {
@@ -246,17 +225,103 @@ $.extend(User.prototype, {
                     }
                 }
                 var _obj = JSON.stringify(obj);
-                that.getAjaxData(User.ajaxLink2, _obj, function () {
+                Login.user.getAjaxData(Login.ajaxLink2, _obj, function () {
                     window.location.href = 'http://' + window.location.hostname + '/login?new_registration=1';
                 });
-
             }
 
         });
+    },
+    fieldValidate: function (valobj, field) {
 
+        var that = this;
+        var error = '';
+        var flag = true;
+        var _val = valobj.val();
+        if (field == 'email') {
+            if (_val == '') {
 
+                error = 'Field email can not be empty';
+                valobj.next('.validation_msg').html();
+                valobj.next('.validation_msg').html(error);
+                valobj.next('.validation_msg').show();
+                flag = false;
+            }
+            else if (that.isEmail(_val) === false) {
+
+                error = 'Please enter a valid email';
+                valobj.next('.validation_msg').html();
+                valobj.next('.validation_msg').html(error);
+                valobj.next('.validation_msg').show();
+                flag = false;
+            }
+
+        }
+        return flag;
 
     },
+    isEmail: function (email) {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    }
+    
+});
+
+$.extend(User, {
+    ajaxLink2: window.location.origin + '/api/v1',
+    roles: ['Registered', 'Unregistered', 'Unverified', 'Deactivated', 'Denied', 'Password Reset'],
+    role: new Role(),
+    login: new Login()
+});
+
+$.extend(User.prototype, {
+    init: function () {
+        var that = this;
+        
+        User.role.init();
+        User.login.init();
+        that.featureSlider();
+        if ($('#userList').length) {
+            that.createUserPage();
+        }
+
+    },
+    
+    featureSlider: function () {
+        if ($('.feature-list').length) {
+            var li_width = 30;
+            $('.feature-list ul li').each(function () {
+                li_width = li_width + $(this).outerWidth();
+            });
+            $('.feature-list ul').css('width', li_width + 'px');
+            $('.next').click(function () {
+
+                var diff = $('.feature-list ul').width() - $(window).width();
+                if (diff > 0) {
+                    $('.feature-list ul').animate({'left': '-' + diff + 'px'}, 500);
+                }
+            });
+            $('.prev').click(function () {
+
+                var diff = $('.feature-list ul').width() - $(window).width();
+                var pos = 0;
+                if (diff > 0) {
+                    var left_pos = $('.feature-list ul').css('left');
+                    left_pos = parseInt(left_pos);
+
+                    if (left_pos == 0) {
+                        pos = left_pos;
+                    }
+                    else {
+                        pos = diff + left_pos;
+                    }
+                    $('.feature-list ul').animate({'left': '+' + pos + 'px'}, 500);
+                }
+            });
+        }
+    },
+    
+    
     createUserPage: function () {
         var that = this;
         that.flag = true;
@@ -280,9 +345,9 @@ $.extend(User.prototype, {
         that.disableUser();
         that.activateUser();
         that.searchUser();
-        that.addFilter();
+        that.addUserFilter();
         that.changeRole();
-        that.sortTable();
+        that.sortUserTable();
 
     },
     disableUser: function () {
@@ -425,7 +490,7 @@ $.extend(User.prototype, {
         }, 500);
 
     },
-    addFilter: function () {
+    addUserFilter: function () {
         var that = this;
         $('#roles_ul').on('change', '.roles', function () {
             var sortoption = that.getSortingOrder();
@@ -477,19 +542,31 @@ $.extend(User.prototype, {
             that.getUserList(_obj);
         });
     },
-    sortTable: function () {
+    sortUserTable: function () {
         var that = this;
         $('.sort_table').click(function () {
             var order_by = $(this).attr('data-order-by');
             var order_type = $(this).attr('data-order-type');
-
-            $('#userList .sort_table').find('.fa').removeClass('fa-caret-up').addClass('fa-caret-down');
-            $(this).find('.fa').removeClass('fa-caret-down').addClass('fa-caret-up');
-            $('#userList .sort_table').attr('data-order-type', 'asc');
-            $(this).attr('data-order-type', 'desc');
-            $('#userList .sort_table').removeClass('active_sort');
-            $(this).addClass('active_sort');
-
+            $('#userList .sort_table').each(function(){
+                var _orderType = $(this).attr('data-order-type');
+                if(_orderType != 'desc'){
+                    $(this).find('.fa').removeClass('fa-caret-up').addClass('fa-caret-down');
+                    $(this).removeClass('active_sort');
+                    $(this).attr('data-order-type', 'asc');
+                }
+            });
+            
+            if(order_type == 'asc'){
+                $(this).find('.fa').addClass('fa-caret-down').removeClass('fa-caret-up');
+                $(this).attr('data-order-type', 'desc');
+                $(this).addClass('active_sort');
+            }
+            else{
+                $(this).find('.fa').removeClass('fa-caret-down').addClass('fa-caret-up');
+                $(this).attr('data-order-type', 'asc');
+                $(this).addClass('active_sort');
+            }
+            
             var checkedroles = that.getSelectedRoles();
             var checkedStatus = that.getSelectedStatus();
             var searchText = $.trim($('#search_user').val());
@@ -508,7 +585,7 @@ $.extend(User.prototype, {
                     filter: true
                 }
             }
-
+            console.log(obj);
             var _obj = JSON.stringify(obj);
             that.getUserList(_obj);
         });
@@ -518,6 +595,7 @@ $.extend(User.prototype, {
         if ($('#userList .active_sort').length) {
             var oderby = $('#userList .active_sort').attr('data-order-by');
             var ordertype = $('#userList .active_sort').attr('data-order-type');
+            ordertype = (ordertype == 'desc' ? 'asc':'desc');
             sortby.push(oderby);
             sortby.push(ordertype);
         }
@@ -691,60 +769,13 @@ $.extend(User.prototype, {
                 }
             }
         });
-    },
-    fieldValidate: function (valobj, field) {
-
-        var that = this;
-        var error = '';
-        var flag = true;
-        var _val = valobj.val();
-        if (field == 'email') {
-            if (_val == '') {
-
-                error = 'Field email can not be empty';
-                valobj.next('.validation_msg').html();
-                valobj.next('.validation_msg').html(error);
-                valobj.next('.validation_msg').show();
-                flag = false;
-            }
-            else if (that.isEmail(_val) === false) {
-
-                error = 'Please enter a valid email';
-                valobj.next('.validation_msg').html();
-                valobj.next('.validation_msg').html(error);
-                valobj.next('.validation_msg').show();
-                flag = false;
-            }
-
-        }
-        return flag;
-
-    },
-    isEmail: function (email) {
-        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-        return regex.test(email);
-    },
-    flipForm: function () {
-        if ($('.flip').length) {
-            $('.flip').click(function () {
-                $('.card').toggleClass('flipped');
-                return false;
-            });
-        }
-    },
-    openMenu: function () {
-        if ($('.open-menu').length) {
-            $('.open-menu').click(function () {
-                var id = $(this).attr('href');
-                $(id).slideToggle();
-                return false;
-            });
-        }
     }
+    
 });
 
 $.extend(Role, {
-    user: new User()
+    user: new User(),
+    login: new Login()
 });
 
 $.extend(Role.prototype, {
@@ -773,6 +804,7 @@ $.extend(Role.prototype, {
         that.searchRoles();
         that.openPopup('.add-popup');
         that.closePopup();
+        that.createRole();
     },
     getRolesList: function (_obj) {
         var that = this;
@@ -918,6 +950,43 @@ $.extend(Role.prototype, {
           $('.popup-wrapper').hide();
           return false;
             
+        });
+    },
+    createRole: function(){
+        var that = this;
+        var error = '';
+        var ajaxLink = User.ajaxLink2;
+        $('#create_role').click(function(){
+            $('.validation_msg').html('');
+            var _roleName = $.trim($('#role_name').val());
+            var _roleDesc = $.trim($('#role_description').val());
+            
+            if(_roleName == ''){
+                errDiv = $('#role_name').next('.validation_msg');
+                error = 'Role Name Field can be empty';
+                errDiv.html(error);
+            }
+            
+            else if(_roleDesc == ''){
+                errDiv = $('#role_description').next('.validation_msg');
+                error = 'Role Description can be empty';
+                errDiv.html(error);
+            }
+            else{
+                var obj = {
+                        head:{
+                            action:"createRole"
+                        },
+                        body:{
+                            name:_roleName,
+                            description: _roleDesc
+                        }
+                    }
+                var _obj = JSON.stringify(obj);
+                user.getAjaxData(ajaxLink, _obj, function(_data){
+                    window.location.href = 'http://'+window.location.hostname+'/roles?new_role='+_data.body.role_id;
+                });
+            }
         });
     }
 });
